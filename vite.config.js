6a104008b757e8014dd0e975d6537b858cf6b1eb
jonathanshon <jonathan.huettinger@stud.edubs.ch>
2025-04-07 22:30:12 +0200

    Changes JSON and Root

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// Replace 'Daily-Games' with your actual repo name
 export default defineConfig({
   plugins: [react()],
+  base: '/Daily-Games/',
 })
